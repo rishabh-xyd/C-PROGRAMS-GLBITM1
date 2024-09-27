350acd5198ce2c8131d60218d4f518d7122d2f44
Rishabh Singh <106950554+rishabh-xyd@users.noreply.github.com>
2024-09-27 11:57:54 +0530

    Update COMPUND_INTERST.C

--- COMPUND_INTERST.C
+++ COMPUND_INTERST.C
@@ -1,16 +1,16 @@
 #include<stdio.h>
-// #include<math.h>
+#include<math.h>
 int main(){
-    float p,r,t,amt,CI;
+    float p,r,t,amt,n,CI;
     printf("\nenter principle:");
     scanf("%f",&p);
     printf("\nenter rate: ");
     scanf("%f",&r);
-    // printf("\nenter number of times interest is compounded per year: ");
-    // scanf("%f",&n);
+    printf("\nenter number of times interest is compounded per year: ");
+    scanf("%f",&n);
     printf("\nenter time (in years): ");
     scanf("%f",&t);
-    amt=(p*r*t)/100;
+    amt=p*pow((1+r/n),n*t);
     printf("\n amt:%f",amt);
     CI=amt-p;
     printf("\ncoumpund interst:%f",CI);
